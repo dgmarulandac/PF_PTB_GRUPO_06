a92cfe0b86dbc89fc8afa3b057d4f93f7af3a1e7
Gabo <gsarmiento101874@gmail.com>
2023-08-09 22:51:55 -0500

    cambios hasta ahora

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -2,6 +2,26 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
     sequelize.define('User', {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+        },
+        displayName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+        },
+        phone: {
+            type: DataTypes.STRING,
+        },
+        email: {
+            type: DataTypes.STRING,
+            unique: true,
+        }
         
     });
 };
