6b0c9a4180db6a18cd5772b441fa7a9869b12f2e
Gabo <gsarmiento101874@gmail.com>
2023-08-09 21:37:34 -0500

    bolierplate completado

--- api/src/db.js
+++ api/src/db.js
@@ -6,7 +6,7 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/boho`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
@@ -30,9 +30,10 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-// const { Recipe, Diet } = sequelize.models;
+const { Event, Role, Sale, User } = sequelize.models;
 
 // Aca vendrian las relaciones
+//TODO: Agregar las relaciones para cada modelo.
 // Recipe.belongsToMany(Diet, {through: "Recipe_Diet"});
 // Diet.belongsToMany(Recipe, {through: "Recipe_Diet"});
 
