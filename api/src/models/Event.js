7ff5197f2db4f47bcfc32eadf0a668f4ba24c62d
dgmarulandac <54507534+dgmarulandac@users.noreply.github.com>
2023-08-09 22:53:48 -0500

    create_atributtes_model

--- api/src/models/Event.js
+++ api/src/models/Event.js
@@ -3,5 +3,57 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
     sequelize.define('Event', {
 
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4, 
+            allowNull: false,
+            primaryKey : true
+          },
+
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            
+          },
+
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+
+        hour: {
+            type: DataTypes.TIME(6),
+            allowNull: false,
+        },
+
+        seats: {
+            type: DataTypes.STRING,
+                    },
+
+        address: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        country: {
+            type: DataTypes.ENUM('Colombia', 'Venezuela', 'Argentina', 'Uruguay'),
+            allowNull: false,
+        },
+
+        image: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        eventType:{
+            type: DataTypes.ENUM('Musical', 'Deportivo', 'Artistico', 'Otro'),
+            allowNull: false,
+
+        }
     });
 };
