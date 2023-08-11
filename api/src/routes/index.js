32d6d26ef084bdcf5dccc98688a195e25d7a2f73
Gabo <gsarmiento101874@gmail.com>
2023-08-11 01:47:30 -0500

    cambie los comments de index.js y ajuste a events

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,5 @@
 const { Router } = require('express');
-//TODO: Agregar la logica de rutas.
 // Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
 const eventRouter = require("./eventRouter");
 const userRouter = require("./userRouter");
 // const saleRouter = require("./saleRouter");
@@ -10,12 +8,7 @@ const router = Router();
 
 router.use("/events", eventRouter);
 router.use("/users", userRouter);
-// router.use("/sale", saleRouter);
-
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
-
-router.use('/', eventRouter);
+// router.use("/sales", saleRouter);
 
 
 module.exports = router;
