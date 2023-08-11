bf1f98e69d928d6910299004b8ff8ced396a8fd8
Gabriel <git@github.com>
2023-08-11 14:23:16 -0500

    modelo de rol completado

--- api/src/models/Role.js
+++ api/src/models/Role.js
@@ -2,6 +2,15 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
     sequelize.define('Role', {
-        
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4, 
+            allowNull: false,
+            primaryKey : true
+        },
+        type: {
+            type: DataTypes.ENUM("Comprador","Vendedor","Administrador"),
+            allowNull: false
+        }
     });
 };
