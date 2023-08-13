78ebccdad25b808346a6e334ceda0f8c51b5ebc6
dgmarulandac <54507534+dgmarulandac@users.noreply.github.com>
2023-08-13 12:29:30 -0500

    url database and config port

--- api/src/db.js
+++ api/src/db.js
@@ -6,15 +6,15 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST, URL_DATABASE,
 } = process.env;
 
-// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/boho`, {
-//   logging: false, // set to console.log to see the raw SQL queries
-//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-// });
-
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/boho`, {
-  logging: false, 
-  native: false, 
+const sequelize = new Sequelize(`${URL_DATABASE}`, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
+
+// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/boho`, {
+//   logging: false, 
+//   native: false, 
+// });
 
 const basename = path.basename(__filename);
 
