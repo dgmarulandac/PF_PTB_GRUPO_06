0f30da7ebd79f80fbeede6e2ef5361fd4dc12537
Cristian-Massa <109998938+Cristian-Massa@users.noreply.github.com>
2023-08-13 16:45:44 -0300

    Update validation.js

--- client/src/functions/Validations/registerValidation/validation.js
+++ client/src/functions/Validations/registerValidation/validation.js
@@ -2,8 +2,8 @@ import axios from "axios";
 const numRegex = /-?\d+(\.\d+)?/; // detectar numeros
 const sepcialRegex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;// detectar caracteres especiales
 const mailRegex = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/;// detectar mail
-const dirRegex = /^(?!.*\b[a-zA-Z]+\s+\(\d+\),\s+[a-zA-Z]+,\s+[a-zA-Z]+\b).+$/ // detectar la direccion que este bien escrita
-function registerValidation (user, password, dir, email, country) {
+const dirRegex = /^[a-zA-Z\s]+ \d+, [a-zA-Z\s]+$/ // detectar la direccion que este bien escrita
+function registerValidation (user, password, dir, email) {
     const errors = [];
     axios.get(`/users/check?displayName=${user}&email=${email}`)
         .then(res => res.data)
