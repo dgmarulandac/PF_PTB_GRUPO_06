bc9af592d0c9aa861429a1a4df6e86bf01aa1cd0
dgmarulandac <54507534+dgmarulandac@users.noreply.github.com>
2023-08-13 12:41:46 -0500

    update index

--- api/index.js
+++ api/index.js
@@ -27,3 +27,7 @@ conn.sync({ force: true }).then(() => {
     console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
 });
+
+app.get('/', (req, res) =>{
+  res.send('Hola Mundo')
+})
