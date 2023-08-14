b56caad1ba18e65638bcb6d24d80c494ec764315
Gabo <gsarmiento101874@gmail.com>
2023-08-14 01:58:33 -0500

    User model, cambie tipo de password a string

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -34,7 +34,7 @@ module.exports = (sequelize) => {
             type: DataTypes.STRING,
         },
         password: {
-            type: DataTypes.BLOB,
+            type: DataTypes.STRING,
             allowNull: false
         }
     });
