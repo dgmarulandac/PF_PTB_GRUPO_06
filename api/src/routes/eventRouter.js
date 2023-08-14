2e2430afbe611e539f0471b3f6f2f5071f5df861
Gabo <gsarmiento101874@gmail.com>
2023-08-14 04:12:04 -0500

    added permisions to routes

--- api/src/routes/eventRouter.js
+++ api/src/routes/eventRouter.js
@@ -1,10 +1,12 @@
 const { Router} = require ("express");
 const {getEventHandler, getEventByIdHandler, postCreateEventHandler}  = require ("../handlers/eventHandler")
 
+const { verifyToken, isAdmin, isSeller } = require("../middleware/authJwt");
+
 const eventRouter = Router ();
 
 eventRouter.get("/", getEventHandler);
 eventRouter.get("/:id",getEventByIdHandler);
-eventRouter.post("/createEvent", postCreateEventHandler);
+eventRouter.post("/createEvent", [verifyToken, isAdmin, isSeller], postCreateEventHandler);
 
 module.exports = eventRouter;
