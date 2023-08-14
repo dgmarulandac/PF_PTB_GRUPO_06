a746719b27679f6dd5ba41c502dfae9f62f29ead
Gabo <gsarmiento101874@gmail.com>
2023-08-14 02:13:46 -0500

    added initialization of roles when the server is kicked off.

--- api/index.js
+++ api/index.js
@@ -26,6 +26,7 @@ const port = process.env.PORT || 3001
 conn.sync({ force: true }).then(() => {
   server.listen(port, () => {
     console.log(`%s listening at ${port}`); // eslint-disable-line no-console
+    //! SOLO PARA DEV Y TESTING EN PRODUCCION NO DEBE IR.
     initial();
   });
 });
