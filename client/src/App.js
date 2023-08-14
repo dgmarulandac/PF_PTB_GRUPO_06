2be772f8a71b622789d6e3f009bd73d52dccd076
Gabo <gsarmiento101874@gmail.com>
2023-08-14 18:37:39 -0500

    cambiadas las importaciones

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,11 @@ import axios from 'axios';
 import { Route, Routes } from 'react-router-dom';
 
 //Components
-import Register from './Components/register/register';
-import Login from './Components/login/login';
-import Home from './Components/Home/Home';
-import FormEvent from './Components/FormEvent/FormEvent'
-import Detail from './Components/Detail/Detail';
+import Home from './components/Home/Home.jsx';
+import Register from './components/register/register.jsx';
+import Login from './components/login/login.jsx';
+import FormEvent from './components/FormEvent/FormEvent.jsx';
+import Detail from './components/Detail/Detail.jsx';
 axios.defaults.baseURL = 'https://pf-grupo06-back.onrender.com/'
 
 
