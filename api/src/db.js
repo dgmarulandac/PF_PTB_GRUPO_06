5bce38ef9923c13d8062b1bd23cb5c08cddd2ff8
dgmarulandac <54507534+dgmarulandac@users.noreply.github.com>
2023-08-14 17:43:21 -0500

    fix back daniel

--- api/src/db.js
+++ api/src/db.js
@@ -6,14 +6,9 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_URL_DEPLOY,
 } = process.env;
 
-const sequelize = new Sequelize(DB_URL_DEPLOY, {
-	logging: false,
-	native: false,
-  dialectOptions: {
-    ssl: {
-      require: true
-    }
-  }
+const sequelize = new Sequelize(`${DB_URL_DEPLOY}`, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
 
 const basename = path.basename(__filename);
