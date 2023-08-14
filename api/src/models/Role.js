edebde7585cf769b90e310864fbe9f626334615c
Gabo <gsarmiento101874@gmail.com>
2023-08-14 02:09:32 -0500

    actualizados los modelos

--- api/src/models/Role.js
+++ api/src/models/Role.js
@@ -4,7 +4,6 @@ module.exports = (sequelize) => {
     sequelize.define('Role', {
         id: {
             type: DataTypes.INTEGER,
-            autoIncrement: true,
             allowNull: false,
             primaryKey : true
         },
