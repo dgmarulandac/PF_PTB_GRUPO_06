8986db2cd8a2f58a25bd3b34a326ab36a33e8da9
Gabo <gsarmiento101874@gmail.com>
2023-08-14 22:33:03 -0500

    added css home

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -14,7 +14,7 @@ export default function Card({ event }) {
             {id &&
                 <Link to={`/detail/${id}`} className={style.container}>
                     <div>
-                        <img src={image} alt="imagen event" width='330'/>
+                        <img src={image} alt="imagen event" width='230'/>
                     </div>
                     <div className={style.info}>
                         <p className={style.location}>{adress} - {country} <span> /{eventType}</span></p>
