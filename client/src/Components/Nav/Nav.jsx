5d3fd81d08339c58f0b241a41c08f3c8b9644374
Daniel Lopez <daniel.l1.g@hotmail.com>
2023-08-14 22:31:40 -0500

    provando css

--- client/src/Components/Nav/Nav.jsx
+++ client/src/Components/Nav/Nav.jsx
@@ -1,13 +1,18 @@
 import { Link } from "react-router-dom"
+import styles from "./Nav.module.css"
+
 
 const Nav = () => {
 
     return(
-    <div >
+    <div className={styles.Nav}>
+        <h3>BOHO</h3>
+        <div>
         <Link to = "/"><button>home</button></Link>
         <Link to = "/login"><button>login</button></Link>
         <Link to = '/register'><button>register</button></Link>
         <Link to = '/createEvent'><button>create event</button></Link>
+        </div>
     </div>
     )
 }
