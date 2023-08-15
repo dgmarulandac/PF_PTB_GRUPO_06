7660b1f17fcc6bb588fb7205a35bd8c50b776348
Gabo <gsarmiento101874@gmail.com>
2023-08-14 22:31:20 -0500

    added css home

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -8,6 +8,7 @@ import { Pagination, Autoplay, Navigation, Keyboard } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
+import styles from './Home.module.css';
 
 export default function Home() {
 
@@ -17,20 +18,6 @@ export default function Home() {
     useEffect( () => {
         dispatch( getAllEvent() );
     }, [] );
-
-    // const evento = {
-    //     id: 90,
-    //     adress: "av de mayo",
-    //     cantTickets: "400",
-    //     country: "Argentina",
-    //     date: "2023-08-26",
-    //     description: "pelea de bobos",
-    //     eventType: "Deportivo",
-    //     hour: "21:00",
-    //     image: "https://i0.wp.com/imgs.hipertextual.com/wp-content/uploads/2023/06/elon-musk-vs-mark-zuckerberg.jpg?fit=1200%2C675&quality=50&strip=all&ssl=1",
-    //     name: "musk vs zuckenberg",
-    //     ticketPrice: "5990"
-    // }
 
     const slides = ['https://www.lunapark.com.ar/images/eventos/eventos/11066.jpg?1680027036', 'https://www.lunapark.com.ar/images/eventos/eventos/11027.jpg?1679514160',
 'https://www.lunapark.com.ar/images/eventos/eventos/11477.jpg?1690910639']
@@ -59,7 +46,7 @@ export default function Home() {
                 })}
             </Swiper>  
             </div>
-            <div>
+            <div className={styles.cards}>
                 {events && events.map( event => {
                     return <Card event={event}/>
                 } )}
