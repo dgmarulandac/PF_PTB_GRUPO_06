82252421aac0ef6fb87405517b34b01c27ed0054
Gabo <gsarmiento101874@gmail.com>
2023-08-14 20:10:27 -0500

    quitada detail

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,7 +2,6 @@ const { Router } = require('express');
 // Importar todos los routers;
 const eventRouter = require("./eventRouter");
 const userRouter = require("./userRouter");
-const detailRoute = require("./detailRouter");
 // const saleRouter = require("./saleRouter");
 
 const router = Router();
