7dd18d9398553950f9f851d7c7e87378f140a10f
Gabo <gsarmiento101874@gmail.com>
2023-08-14 21:48:08 -0500

    nav bar sin css

--- client/src/Components/Nav/Nav.jsx
+++ client/src/Components/Nav/Nav.jsx
@@ -4,7 +4,10 @@ const Nav = () => {
 
     return(
     <div >
-        <link rel="stylesheet" href="" />
+        <Link to = "/"><button>home</button></Link>
+        <Link to = "/login"><button>login</button></Link>
+        <Link to = '/register'><button>register</button></Link>
+        <Link to = '/createEvent'><button>create event</button></Link>
     </div>
     )
 }
