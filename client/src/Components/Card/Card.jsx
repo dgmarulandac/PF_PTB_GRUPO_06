acbded664102959df578508e5bc54ddec9c8bebe
Gabo <gsarmiento101874@gmail.com>
2023-08-15 12:14:43 -0500

    ajusatdas las imagenes de la tarjeta

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -14,7 +14,7 @@ export default function Card({ event }) {
             {id &&
                 <Link to={`/event/${id}`} className={style.container}>
                     <div>
-                        <img src={image} alt="imagen event" width='230'/>
+                        <img className={style.image} src={image} alt="imagen event"/>
                     </div>
                     <div className={style.info}>
                         <p className={style.location}>{adress} - {country} <span> /{eventType}</span></p>
