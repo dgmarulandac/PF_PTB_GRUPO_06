d391210050716283db7b6d63530bc8f340fa1836
Cecilia Castellano <emiriritey@gmail.com>
2023-08-15 00:32:03 -0400

    ajustes del package

--- client/src/Redux/Action/action.js
+++ client/src/Redux/Action/action.js
@@ -3,14 +3,14 @@ import { GET_ALL_EVENT, CREATE_EVENT, GET_DETAIL, FILTER_GET_EVENTS } from "./ac
 import Swal from "sweetalert2";
 
 export const getAllEvent = ()=>{
-    return async (dispatch)=>{
-        const {data} = await axios.get(`/events`)
-        return dispatch({
-            type: GET_ALL_EVENT,
-            payload: data
-        })
-    }
-}
+    return function(dispatch){
+        axios.get('/events')
+        .then(data=>dispatch({type: GET_ALL_EVENT, payload: data.data}))
+        .catch( reason => {
+            dispatch({type: GET_ALL_EVENT, payload: []})
+        });
+    }; 
+};
 
 export const createEvent = (event)=>{
     return async (dispatch)=>{
