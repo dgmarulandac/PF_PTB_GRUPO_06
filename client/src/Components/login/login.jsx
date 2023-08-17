a7b7a6cfecb84d8e2b8ac644271365263bebc632
Gabo <gsarmiento101874@gmail.com>
2023-08-17 18:24:50 -0500

    updated login to route via redux

--- client/src/Components/login/login.jsx
+++ client/src/Components/login/login.jsx
@@ -1,15 +1,16 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import userValidations from "../../functions/Validations/loginValidation/validation";
 import { useSelector, useDispatch } from "react-redux";
+import { postLogin } from "../../Redux/Action/action";
 import video from "../../Utils/videos/backgroundLogin.mp4"
 import styles from './login.module.css'
 
 export default function Login() {
-    const userSesion = useSelector(state => state.user)
+    const dispatch = useDispatch();
+    const userSesion = useSelector(state => state.userSesion)
     const navigate = useNavigate()
-    const [users, setUsers] = useState({
+    const [user, setUsers] = useState({
         displayName: '',
         password: ''
     })
@@ -19,32 +20,25 @@ export default function Login() {
         const value = e.currentTarget.value
         const name = e.currentTarget.name
         setUsers({
-            ...users,
+            ...user,
             [name]: value
         })
     };
+
     function handleSubmit(e) {
         e.preventDefault();
-        const { displayName, password } = users
+        const { displayName, password } = user
         const errores = userValidations(displayName, password);
         setErrors(errores);
         if( errores.length === 0 ) {
-            axios.post(`/users/login`, users)
-                .then(data => {
-                    console.log(data.data);
-                    setResult(true);
-                })
-                .catch( reason => {
-                    console.log(reason);
-                    setResult(false);
-                });
+            dispatch(postLogin(user));
         }
     };
     useEffect(() => {
-        if (result) {
+        if (userSesion) {
             navigate('/')
         }
-    }, [result])
+    }, [userSesion])
     return (
         <div className={styles.Background}>
                             <div className={styles.titleContainer}>
