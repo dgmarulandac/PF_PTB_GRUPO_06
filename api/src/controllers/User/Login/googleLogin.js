3283964d4f00e3df1f85cdf09dcc987a32cac5ec
Gabo <gsarmiento101874@gmail.com>
2023-08-17 22:48:21 -0500

    adde login via google

---
+++ api/src/controllers/User/Login/googleLogin.js
@@ -0,0 +1,27 @@
+const { User } = require("../../../db");
+
+const googleLogin = async (email) => {
+
+    const candidateUser = await User.findOne({where: { email: email }});
+
+    const token = jwt.sign({id: candidateUser.id}, SECRET, {
+        algorithm: 'HS256',
+        allowInsecureKeySizes: false,
+        expiresIn: 604800, // 7 days
+    });
+
+    let roles = await candidateUser.getRoles();
+    roles = roles.map( role => role.type );
+
+    return {
+        id: candidateUser.id,
+        displayName: candidateUser.displayName,
+        email: candidateUser.email,
+        name: candidateUser.name,
+        image: candidateUser.image,
+        roles: roles,
+        jwt: token
+    };
+};
+
+module.exports = googleLogin;
--- api/src/controllers/User/postUserLogin.js
+++ api/src/controllers/User/postUserLogin.js
@@ -1,52 +1,32 @@
 const { User } = require("../../db");
 const jwt = require("jsonwebtoken");
-const {SECRET} = process.env;
-const bcrypt = require("bcryptjs");
+const crypto = require("crypto");
+const postUser = require("./postUser");
+
+const bohoLogin = require("./Login/bohoLogin");
+const googleLogin = require("./Login/googleLogin");
 
 const postUserLogin = async ( user ) => {
 
     if( user.platform === "boho" ) {
-        if( !user.displayName && !user.password ) {
-            throw Error("Datos de ususario incorrectos.")
-        }
-
-        const candidateUser = await User.findOne({where: { displayName: user.displayName }});
-
-        if( candidateUser === null ) {
-            throw Error('El usuario no existe, registrate antes de iniciar sesión.');
-        }
-
-        const validation = bcrypt.compareSync( user.password, candidateUser.password );
-
-        if( !validation ) {
-            throw Error('Contraseña Incorrecta.');
-        }
-
-        const token = jwt.sign({id: candidateUser.id}, SECRET, {
-            algorithm: 'HS256',
-            allowInsecureKeySizes: false,
-            expiresIn: 604800, // 7 days
-        });
-
-        let roles = await candidateUser.getRoles();
-        roles = roles.map( role => role.type );
-
-        return {
-            id: candidateUser.id,
-            displayName: candidateUser.displayName,
-            email: candidateUser.email,
-            name: candidateUser.name,
-            image: candidateUser.image,
-            roles: roles,
-            jwt: token
-        };
+        
+        return bohoLogin(user);
 
     } else if( user.platform === 'google' ) {
         
-        jwt.decode(user.jwt);
+        const decodedUser = jwt.decode(user.jwt);
 
-        // Validate token google.
-        // check if user exists
+        console.log(decodedUser);
+        
+        const candidateUser = await User.findOne({where: { email: decodedUser.email }});
+
+        if( !candidateUser ) {
+            const password = crypto.randomBytes(25).toString('hex');
+            const userCreate = { password, email: decodedUser.email, name: decodedUser.name, displayName: decodedUser.email, image: decodedUser.picture };
+            postUser(userCreate);
+        }
+
+        return googleLogin(decodedUser.email);
 
     } else {
         throw Error('Plataforma indefinida, error al iniciar sesión.')
