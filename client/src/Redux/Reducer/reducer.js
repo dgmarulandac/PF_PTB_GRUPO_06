83223a0d2856d4c1fc1019eb5f7dcf5a2ab55870
Héctor Luis Rivero <hectorluisrivero14@gmail.com>
2023-08-17 21:05:09 -0300

    archivos bases de tailwind y component modal

--- client/src/Redux/Reducer/reducer.js
+++ client/src/Redux/Reducer/reducer.js
@@ -1,10 +1,11 @@
-import { GET_ALL_EVENT, CREATE_EVENT, GET_DETAIL, FILTER_GET_EVENTS } from "../Action/action-type";
+import { GET_ALL_EVENT, CREATE_EVENT, GET_DETAIL, FILTER_GET_EVENTS, MODAL } from "../Action/action-type";
 
 const initialState = {
     country: ['Colombia', 'Venezuela', 'Argentina', 'Uruguay'],
     eventTypes: ['Musical', 'Deportivo', 'Artistico', 'Otro'],
     events: [],
     detail: {},
+    modalOn: false
 }
 
 const rootReducer = (state = initialState, action)=>{
@@ -16,11 +17,16 @@ const rootReducer = (state = initialState, action)=>{
             return {...state, events: [...state.events, payload]};
         case GET_DETAIL:
             return {...state, detail: payload};
-            case FILTER_GET_EVENTS: //Para filtrar eventos
+        case FILTER_GET_EVENTS: //Para filtrar eventos
 			return {
 				...state,
 				events: action.payload,
 			};
+        case MODAL:
+            return {
+                ...state,
+                modalOn: payload
+            }
             
         default: return state;
     }
