f670b3a2124235e6b64358a80b963d9f68975dab
Gabo <gsarmiento101874@gmail.com>
2023-08-17 19:45:56 -0500

    fixed logic for standard boho login

--- api/src/controllers/User/postUserLogin.js
+++ api/src/controllers/User/postUserLogin.js
@@ -4,35 +4,51 @@ const {SECRET} = process.env;
 const bcrypt = require("bcryptjs");
 
 const postUserLogin = async ( user ) => {
-    const candidateUser = await User.findOne({where: { displayName: user.displayName }});
 
-    if( candidateUser === null ) {
-        throw Error('The user does not exist, please sign up.');
+    if( user.platform === "boho" ) {
+        if( !user.displayName && !user.password ) {
+            throw Error("Datos de ususario incorrectos.")
+        }
+
+        const candidateUser = await User.findOne({where: { displayName: user.displayName }});
+
+        if( candidateUser === null ) {
+            throw Error('El usuario no existe, registrate antes de iniciar sesión.');
+        }
+
+        const validation = bcrypt.compareSync( user.password, candidateUser.password );
+
+        if( !validation ) {
+            throw Error('Contraseña Incorrecta.');
+        }
+
+        const token = jwt.sign({id: candidateUser.id}, SECRET, {
+            algorithm: 'HS256',
+            allowInsecureKeySizes: false,
+            expiresIn: 604800, // 7 days
+        });
+
+        let roles = await candidateUser.getRoles();
+        roles = roles.map( role => role.type );
+
+        return {
+            id: candidateUser.id,
+            displayName: candidateUser.displayName,
+            email: candidateUser.email,
+            name: candidateUser.name,
+            roles: roles,
+            jwt: token
+        };
+
+    } else if( user.platform === 'google' ) {
+        
+        console.log(user.jwt);
+        console.log(jwt.decode(user.jwt));
+
+    } else {
+        throw Error('Plataforma indefinida, error al iniciar sesión.')
     }
 
-    const validation = bcrypt.compareSync( user.password, candidateUser.password );
-
-    if( !validation ) {
-        throw Error('Invalid Password.');
-    }
-
-    const token = jwt.sign({id: candidateUser.id}, SECRET, {
-        algorithm: 'HS256',
-        allowInsecureKeySizes: false,
-        expiresIn: 604800, // 7 days
-    });
-
-    let roles = await candidateUser.getRoles();
-    roles = roles.map( role => role.type );
-    
-    return {
-        id: candidateUser.id,
-        displayName: candidateUser.displayName,
-        email: candidateUser.email,
-        name: candidateUser.name,
-        roles: roles,
-        accessToken: token
-    };
 };
 
 module.exports = postUserLogin;
