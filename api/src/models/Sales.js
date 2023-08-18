18d418a24d802aaa4ff1d2fee094dd6319cdb3bd
dgmarulandac <54507534+dgmarulandac@users.noreply.github.com>
2023-08-17 21:09:40 -0500

    update sales

--- api/src/models/Sales.js
+++ api/src/models/Sales.js
@@ -10,7 +10,7 @@ module.exports = (sequelize) => {
           },
 
         idOrder:{
-            type: DataTypes.INTEGER,
+            type: DataTypes.STRING,
             allowNull:false
         },
 
