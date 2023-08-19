06910fb7ffc6e249442066045495f295e5c5ff7e
Gabo <gsarmiento101874@gmail.com>
2023-08-19 14:58:07 -0500

    postAuth controller comments

--- api/src/controllers/User/postAuth.js
+++ api/src/controllers/User/postAuth.js
@@ -1,7 +1,5 @@
 const { User } = require("../../db");
 const jwt = require("jsonwebtoken");
-const crypto = require("crypto");
-const postUser = require("./postUser");
 
 postAuth = async (jwt) => {
     // verify jwt
