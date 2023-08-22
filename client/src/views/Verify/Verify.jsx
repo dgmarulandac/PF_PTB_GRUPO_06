d5487448585e45bd0ecfe1748b2bd534c5a4c04b
Cecilia Castellano <emiriritey@gmail.com>
2023-08-21 23:22:55 -0400

    cambios

--- client/src/views/Verify/Verify.jsx
+++ client/src/views/Verify/Verify.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 import styles from "./Verify.module.css";
+//import { styles } from "./verifyStyle";
 import axios from "axios";
 
 const Verify = () => {
@@ -42,6 +43,10 @@ const Verify = () => {
 
 	return (
 		<div className={styles.containerP}>
+			<div className={styles.header}>
+                <h1 className={styles.logo}>BOHO</h1>
+                <p className={styles.p}>¡¡Restablece tu Contraseña!!</p>
+            </div>
 			<div className={styles.headerP}>Verificación</div>
 			<form className={styles.formP} onSubmit={handleVerify}>
 				<div className={styles.TextFormP}>Usuario</div>
---
+++ client/src/views/Verify/verifyStyle.js
@@ -0,0 +1,20 @@
+// export const styles = {
+//     body:"grid justify-center bg-whithe dark:bg-fondoDark",
+//     input: "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white",
+//     container: "m-5 w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700",
+//     form: "space-y-1",
+//     label: "block mb-2 text-sm font-medium text-gray-900 dark:text-white",
+//     h5: "text-xl font-medium text-gray-900 dark:text-white",
+//     dual: "grid grid-cols-2 gap-4 justify-center",
+//     input_Dual:"bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white",
+//     button: 'mt-3 w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800',
+//     select: "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
+//     div_ind: "grid grid-rows-divForm m-0",
+//     error: "text-red-500 text-sm sticky self-end",
+//     exito: "text-sm text-lime-400",
+//     div_g: "grid grid-rows-divDesImg",
+//     div_p: "grid grid-rows-divForm m-0",
+//     p: "font-medium text-gray-900 dark:text-white border-b-[3px] p-2",
+//     header: "mt-5 mr-5 ml-5 mb-0 w-full max-w-sm p-4 sm:p-6 md:p-8 dark:text-white",
+//     logo: 'font-bungee text-5xl'
+// }
