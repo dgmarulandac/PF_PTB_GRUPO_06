ffdef2192c368dd6063b009e76652bc844225b45
Gabo <gsarmiento101874@gmail.com>
2023-08-22 19:08:30 -0500

    force true

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const {initial} = require('./src/rolesSpec.js')
 const port = process.env.PORT || 3001
 
 // Syncing all the models at once.
-conn.sync({ alter: true }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(port, () => {
     console.log(`%s listening at ${port}`); // eslint-disable-line no-console
     //! SOLO PARA DEV Y TESTING EN PRODUCCION NO DEBE IR.
