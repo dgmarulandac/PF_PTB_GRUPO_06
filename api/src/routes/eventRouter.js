587b4df98bd5cc7426bdc05b570b4beb287c72d1
Gabo <gsarmiento101874@gmail.com>
2023-08-23 00:55:06 -0500

    added autorization to events

--- api/src/routes/eventRouter.js
+++ api/src/routes/eventRouter.js
@@ -1,13 +1,13 @@
 const { Router} = require ("express");
 const {getEventHandler, getEventByIdHandler, postCreateEventHandler, putEventHandler, getMyEventHandler}  = require ("../handlers/eventHandler")
 
-
+const { verifyToken, isAdmin, isSeller } = require("../middleware/authJwt");
 const eventRouter = Router();
 
 eventRouter.get("/", getEventHandler);
-eventRouter.post("/createEvent", postCreateEventHandler);
+eventRouter.post("/createEvent", [verifyToken, isSeller], postCreateEventHandler);
 eventRouter.get("/:id",getEventByIdHandler);
-eventRouter.put("/updateEvent/:id",putEventHandler);
-eventRouter.get("/myEvents/:id", getMyEventHandler)
+eventRouter.put("/updateEvent/:id", [verifyToken, isSeller], putEventHandler);
+eventRouter.get("/myEvents/:id", [verifyToken, isSeller], getMyEventHandler)
 
 module.exports = eventRouter;
