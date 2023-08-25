6c7af081222e2f4c21d5840b1a670256f5633301
Gabo <gsarmiento101874@gmail.com>
2023-08-25 17:09:50 -0500

    added Month Sorting

--- api/src/controllers/Sale/getMySales.js
+++ api/src/controllers/Sale/getMySales.js
@@ -10,11 +10,11 @@ const getMySales = async (idSeller) => {
     }
 
     let orders = [];
-    const monthYears = [];
+    let monthYears = [];
 
     for( let i = 0; i < events.length; i++ ) {
         const date = new Date(events[i].date);
-        const myString = `${date.getMonth()}-${date.getFullYear()}`;
+        const myString = `${(''+date.getMonth()).length == 1 ? '0'+String(date.getMonth()) : date.getMonth()}-${date.getFullYear()}`;
         if( monthYears.filter(my => {return my === myString }).length === 0 ) {
             monthYears.push(myString);
         }
@@ -26,6 +26,7 @@ const getMySales = async (idSeller) => {
     }
 
     const sales = [];
+    monthYears = monthYears.sort(compareMonths);
 
     for( let i = 0; i < monthYears.length; i++ ) {
         const objSale = {month: monthYears[i], sales: 0};
@@ -40,5 +41,27 @@ const getMySales = async (idSeller) => {
 
     return sales;
 };
+
+function compareMonths( a,b ) {
+    const aMonth = parseInt(a.split('-')[0]);
+    const aYear = parseInt(a.split('-')[1]);
+
+    const bMonth = parseInt(b.split('-')[0]);
+    const bYear = parseInt(b.split('-')[1]);
+
+    if( aYear < bYear ) {
+        return -1;
+    } else if ( bYear < aYear ) {
+        return 1;
+    } else {
+        if( aMonth < bMonth ) {
+            return -1;
+        } else if( bMonth < aMonth ) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
 
 module.exports = getMySales;
