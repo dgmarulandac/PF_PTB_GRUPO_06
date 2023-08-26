838efb0839a0275a6faaaf4ae6bf42f49d7fe595
dgmarulandac <54507534+dgmarulandac@users.noreply.github.com>
2023-08-26 17:24:27 -0500

    model create

--- api/src/models/Review.js
+++ api/src/models/Review.js
@@ -2,6 +2,35 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
     sequelize.define('Review', {
+
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4, 
+            allowNull: false,
+            primaryKey : true
+          },
+
+        score: {
+            type: DataTypes.INTEGER,
+            allowNull:false,
+        },
+
+        comment: {
+            type:DataTypes.STRING(1234),
+            allowNull: false,
+        },
+
+        approved: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+
+        userId: {
+            type:DataTypes.UUID,
+            allowNull: false,
+        }
+
+
         
     });
 };
