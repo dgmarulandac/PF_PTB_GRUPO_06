07df2065d831dcb6a1042bc6564ab3f6e95e2a72
Giann92 <56614039+Giann92@users.noreply.github.com>
2023-08-28 18:45:41 -0300

    SalesChart_Mas_info

--- client/src/Components/EventsDashboard/EventsDashboard.jsx
+++ client/src/Components/EventsDashboard/EventsDashboard.jsx
@@ -3,15 +3,36 @@ import { useSelector, useDispatch } from "react-redux";
 import { getMyEvents, getMySales } from "../../Redux/Action/action";
 import { Link } from "react-router-dom";
 import Card from "../Card/Card";
-import SalesChart from "../SalesChart/SalesChart";
+import { DailySalesChart, SalesChart } from "../SalesChart/SalesChart";
 import * as styles from "./EventDashboardStiles";
+import stylesSales from "../SalesChart/SalesChart.module.css"
 
 
 const EventsDashboard = () => {
     const dispatch = useDispatch();
     const user = useSelector((state) => state.userSesion);
     const events = useSelector((state) => state.myEvents);
-    const sales = useSelector((state) => state.mySales);
+    // const sales = useSelector((state) => state.mySales);
+    const sales = [
+        {
+            month: "Julio",
+            dailySales: [
+                { day: "Lunes", sales: 900, cantTickets: 2000, price: 10 },
+                { day: "Martes", sales: 100, cantTickets: 1000, price: 10 },
+                { day: "Miercoles", sales: 350, cantTickets: 1000, price: 10 }
+            ]
+        },
+        {
+            month: "Agosto",
+            dailySales: [
+                { day: "Lunes", sales: 800, cantTickets: 2000, price: 10 },
+                { day: "Martes", sales: 0, cantTickets: 1200, price: 10 },
+                { day: "Miercoles", sales: 250, cantTickets: 1200, price: 10 }
+            ]
+        },
+        // ...otros meses
+    ];
+
 
     useEffect(() => {
         dispatch(getMyEvents(user.id));
@@ -19,11 +40,12 @@ const EventsDashboard = () => {
     }, []);
 
 
-    return(
+    return (
         <div>
             <h1 className={styles.error}>Mis Ventas</h1>
-            <div>
+            <div className={stylesSales.chartWrapper}>
                 <SalesChart sales={sales && sales} />
+                <DailySalesChart dailySales={sales[0].dailySales} />
             </div>
             <h1 className={styles.error}>Mis Eventos</h1>
             <div className={styles.cardcontainer}>
@@ -35,9 +57,9 @@ const EventsDashboard = () => {
                 ) : (
                     events.map((event) => {
                         return <div>
-                                <Card event={event} key={event.id} />
-                                <Link to={`/editEvent/${event.id}`}><button className={styles.button}>Editar Evento</button></Link>
-                            </div>;
+                            <Card event={event} key={event.id} />
+                            <Link to={`/editEvent/${event.id}`}><button className={styles.button}>Editar Evento</button></Link>
+                        </div>;
                     })
                 )}
             </div>
--- client/src/Components/SalesChart/SalesChart.jsx
+++ client/src/Components/SalesChart/SalesChart.jsx
@@ -3,47 +3,108 @@ import { VictoryBar, VictoryChart, VictoryAxis, VictoryTooltip } from 'victory';
 import styles from './SalesChart.module.css';
 
 const SalesChart = ({ sales }) => {
-  const totalSales = sales.reduce((total, data) => total + data.sales, 0);
+  // Calculate monthly total sales and profit by accumulating daily sales and profit
+  const monthlyData = sales.map(data => ({
+    month: data.month,
+    totalSales: data.dailySales.reduce((total, dailyData) => total + dailyData.sales, 0),
+    totalProfit: data.dailySales.reduce((total, dailyData) => total + (dailyData.sales * dailyData.price), 0),
+  }));
+
+  const totalProfit = monthlyData.reduce((total, data) => total + data.totalProfit, 0);
 
   return (
     <div className={styles.salesChartContainer}>
-      <h2 className={styles.salesChartTitle}>Gráfico de Ventas</h2>
+      <h2 className={styles.salesChartTitle}>Gráfico de Ventas Mensual</h2>
       <div className={styles.salesChart}>
         <VictoryChart domainPadding={20}>
           <VictoryAxis
-            tickValues={sales.map(data => data.month)}
+            tickValues={monthlyData.map(data => data.month)}
             tickFormat={(tick) => tick}
           />
           <VictoryAxis dependentAxis />
           <VictoryBar
-            data={sales}
+            data={monthlyData}
             x="month"
-            y="sales"
-            labels={({ datum }) => `${datum.sales} ventas (${((datum.sales / totalSales) * 100).toFixed(2)}%)`}
+            y="totalProfit" // Use totalProfit for the y-axis
+            labels={({ datum }) => `${datum.totalProfit} ganancia\n(${((datum.totalProfit / totalProfit) * 100).toFixed(2)}%)`}
             labelComponent={<VictoryTooltip />}
             style={{
               data: { fill: '#8884d8' },
             }}
           />
         </VictoryChart>
-        
-      </div> 
+      </div>
       
       <div className={styles.salesInfo}>
         <h3>Detalles: </h3>
-        {sales.map(data => (
-          <div key={data.month} className={styles.salesData}>
-            <span className={styles.month}>{data.month} - </span>
-            <span className={styles.salesValue}>{data.sales} - </span>
-            <span className={styles.percentage}>{((data.sales / totalSales) * 100).toFixed(2)}%</span>
+        {monthlyData.map(data => (
+          <div key={`details-${data.month}`} className={styles.salesData}>
+            <span className={styles.month}>Mes: {data.month}</span>
+            <span className={styles.salesValue}>Total Ganancia: ${data.totalProfit}</span>
+            <span className={styles.percentage}>
+              {totalProfit === 0 ? '0.00%' : `${((data.totalProfit / totalProfit) * 100).toFixed(2)}%`}
+            </span>
           </div>
         ))}
-      </div> 
+      </div>
     </div>
   );
 };
 
-export default SalesChart;
+const DailySalesChart = ({ dailySales }) => {
+  // Calculate total profit by accumulating daily sales and profit
+  const totalDailyProfit = dailySales.reduce((total, data) => total + (data.sales * data.price), 0);
+
+  return (
+    <div className={styles.salesChartContainer}>
+      <h2 className={styles.salesChartTitle}>Gráfico de Ventas Diarias</h2>
+      <div className={styles.salesChart}>
+        <VictoryChart domainPadding={20}>
+          <VictoryAxis
+            tickValues={dailySales.map(data => data.day)}
+            tickFormat={(tick) => tick}
+          />
+          <VictoryAxis dependentAxis />
+          <VictoryBar
+            data={dailySales}
+            x="day"
+            y="sales"
+            labels={({ datum }) => `${datum.sales} ventas\n(${((datum.sales * datum.price) / totalDailyProfit * 100).toFixed(2)}%)`}
+            labelComponent={<VictoryTooltip />}
+            style={{
+              data: { fill: '#8884d8' },
+            }}
+          />
+        </VictoryChart>
+      </div>
+      
+      <div className={styles.salesInfo}>
+        <h3>Detalles: </h3>
+        {dailySales.map((data, index) => (
+          <div key={data.day} className={styles.salesData}>
+            {index === 0 || dailySales[index - 1].day !== data.day ? (
+              <span className={styles.day}>Día: {data.day}</span>
+            ) : null}
+            <span className={styles.salesValue}>Vendidas: {data.sales}</span>
+            <span className={styles.ganancia}>Ganancia: ${data.sales * data.price}</span>
+            <span className={styles.percentage}>
+              {((data.sales * data.price / totalDailyProfit) * 100).toFixed(2)}%
+            </span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export { SalesChart, DailySalesChart };
+
+
+
+
+
+
+
 
 
 
