f769bf92d997b9adb07cc72eff98fc25fca3cd0f
Cecilia Castellano <emiriritey@gmail.com>
2023-08-29 10:25:44 -0400

    agregado de item precio

--- api/src/handlers/eventHandler.js
+++ api/src/handlers/eventHandler.js
@@ -5,9 +5,9 @@ const {Event} = require('../db.js');
 
 
 const getEventHandler = async (req, res) => {
-    const { name, eventType, country, date, order, sortOrder } = req.query;
+    const { name, eventType, country, date, ticketPrice, order, sortOrder } = req.query;
     try {
-        const events = await getEventFilterController(name, eventType, country, date, order, sortOrder);
+        const events = await getEventFilterController(name, eventType, country, date, ticketPrice, order, sortOrder);
         res.status(200).json(events);
     } catch (error) {
         res.status(404).send(error.message);
