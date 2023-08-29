89b4c685c7a5f8aa70122d85fab0a5c7092c0656
Daniel Lopez <daniel.l1.g@hotmail.com>
2023-08-28 19:20:01 -0500

    para renderizar la review en el front

--- api/src/controllers/Event/getEventById.js
+++ api/src/controllers/Event/getEventById.js
@@ -1,7 +1,7 @@
-const { Event } = require("../../db");
+const { Event, Review } = require("../../db");
 
 const getEventById = async (id) => {
-    const event = await Event.findByPk(id);
+    const event = await Event.findByPk(id,{include:[{model:Review}]});
     return event;
 };
 
