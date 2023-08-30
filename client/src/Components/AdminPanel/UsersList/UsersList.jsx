d3c746d0b565070f26458318eb7828bb85f03d2e
Cristian-Massa <cristianmassa30@gmail.com>
2023-08-30 19:12:25 -0300

    handling user ban

--- client/src/Components/AdminPanel/UsersList/UsersList.jsx
+++ client/src/Components/AdminPanel/UsersList/UsersList.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom"
 import axios from "axios";
 const UsersList = () =>{
     const [users, setUsers] = useState([]);
+    const [ user, setUser ] = useState({})
     const [errors, setErrors] = useState(null);
     const [ success, setSuccess ] = useState(null);
     useEffect(()=>{
@@ -16,12 +17,24 @@ const UsersList = () =>{
 
     const handleChange = (e) => {
         const id = e.currentTarget.value;
-        // Este activa y desactiva el usuario dependiendo si estaba o no activado antes.
-        axios.put(`/users/toggleUser/${id}`,{ headers: { 'X-Access-Token': localStorage.getItem('jwt') } })
-            .then(res => res.data)
-            .then(data => {
-                setSuccess('Listo')
-            })
+        axios.get(`/users/${id}`,{ headers: { 'X-Access-Token': localStorage.getItem('jwt') } })
+        .then(res => res.data)
+        .then(data =>{
+            setUser(data)
+        })
+        .catch(setErrors('no se pudo encontrar ningun usuario') )
+        setUser({
+            ... user,
+            active: !user.active
+        })
+        if(user.displayName){
+            // Este activa y desactiva el usuario dependiendo si estaba o no activado antes.
+            axios.put(`/users/toggleUser/${id}`,{ headers: { 'X-Access-Token': localStorage.getItem('jwt') } })
+                .then(res => res.data)
+                .then(data => {
+                    setSuccess('Listo')
+                })
+        }
     };
     return(
         <div>
