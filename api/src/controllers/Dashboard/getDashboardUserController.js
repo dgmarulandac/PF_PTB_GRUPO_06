94e58723f2d15d64a6c3671fc12e27c645a95bec
Gabo <gsarmiento101874@gmail.com>
2023-08-29 22:03:36 -0500

    cambios

--- api/src/controllers/Dashboard/getDashboardUserController.js
+++ api/src/controllers/Dashboard/getDashboardUserController.js
@@ -4,9 +4,12 @@ const getDashboardUserController = async (id) => {
     try {
         const orders = await Order.findAll({
             where: { idBuyer: id },
+            attributes: ['quantity', 'price', 'createdAt'],
             include: [
-                { model: Sale, required: false }, // false para que tome la venta aun sin que tenga que volver al sitio
-                { model: Event, required: true }
+                { model: Sale, required: true, attributes: [] }, // false para que tome la venta aun sin que tenga que volver al sitio
+                { model: Event, required: true,
+                    attributes: ['name', 'date', 'hour', 'image', 'address', 'country', 'ticketPrice', 'currency']
+                }
             
             ]
         });
