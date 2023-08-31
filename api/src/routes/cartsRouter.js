38555e509de632c01436e570b082a8bbae39e919
Gabo <gsarmiento101874@gmail.com>
2023-08-30 22:36:38 -0500

    added validations

--- api/src/routes/cartsRouter.js
+++ api/src/routes/cartsRouter.js
@@ -1,12 +1,12 @@
 const {Router} = require ("express");
 const {getCartsHandler, postCartsHandler, getCartTokenHandler, putCartTokenHandler} = require ('../handlers/cartHandler');
 
-
+const { verifyTokenOptional, verifyToken, isAdmin } = require("../middleware/authJwt");
 const cartsRouter = Router();
 
-cartsRouter.get("/",  getCartsHandler);
-cartsRouter.post("/createCart", postCartsHandler);
-cartsRouter.get("/:token", getCartTokenHandler)
-cartsRouter.put("/updateCart", putCartTokenHandler)
+cartsRouter.get("/", [verifyToken, isAdmin], getCartsHandler);
+cartsRouter.post("/createCart", [verifyTokenOptional], postCartsHandler);
+cartsRouter.get("/:token", [verifyTokenOptional], getCartTokenHandler)
+cartsRouter.put("/updateCart", [verifyTokenOptional], putCartTokenHandler)
 
 module.exports = cartsRouter;
