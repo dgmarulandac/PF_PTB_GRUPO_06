5fc2b4992a50d1415a539e1a58db5a0fffa064c2
Gabo <gsarmiento101874@gmail.com>
2023-09-02 15:42:27 -0500

    cambios iniciales

--- api/src/controllers/Orders/postOrder.js
+++ api/src/controllers/Orders/postOrder.js
@@ -1,63 +1,51 @@
 require('dotenv').config();
 const {Order, Event} = require('../../db.js');
+const getCartToken = require('../Carts/getCartToken.js');
 var mercadopago = require('mercadopago');
 const {MERCADOPAGO_TOKEN} = process.env;
 
-const postCreateOrder = async (req, res) => {
-    try {
-        const { 
-            idBuyer, 
-            quantity, 
-            price, 
-            idEvent, 
-            
-        } = req.body;
+const postCreateOrder = async (idBuyer, token) => {
 
-        
-        if (!idBuyer || !quantity || !price || !idEvent) {
-            return res.status(400).json({ error: "Faltan campos obligatorios" });
-        }
-
-        mercadopago.configure({
-            access_token: `${MERCADOPAGO_TOKEN}`,
-            
-        });
-    
-        const result = await mercadopago.preferences.create({
-            items:[
-                {
-                    title: Event.findByPk(idEvent).name,
-                    unit_price: price,
-                    currency_id: Event.findByPk(idEvent).currency,
-                    quantity: quantity,
-                }
-            ],
-            back_urls: {
-                success: "https://pf-grupo06-back.onrender.com/orderMercadoPago/success",
-                failure: "https://pf-grupo06-back.onrender.com/orderMercadoPago/failure",
-                pending: "https://pf-grupo06-back.onrender.com/orderMercadoPago/pending"
-            },
-            notification_url: "https://6172-186-113-173-20.ngrok.io/webHook"
-    
-        });
-
-        const createOrder = await Order.create({
-            idBuyer, 
-            quantity, 
-            price, 
-            idEvent,
-            idMercadoPago: result.body.id
-        });
-    
-        console.log(result)
-           
-        return {response: result.response,
-                preferenceId: result.preferenceId}
-        
-    } catch (error) {
-        
-        res.status(500).json({ error: "Hubo un error al crear la orden: " + error.message });
+    if (!idBuyer || !token) {
+        throw new Error("Faltan campos obligatorios");
     }
+
+    //Obtener el carrito del token
+    cart = getCartToken(token);
+    console.log(cart);
+
+    mercadopago.configure({
+        access_token: `${MERCADOPAGO_TOKEN}`,
+    });
+
+    const result = await mercadopago.preferences.create({
+        items:[
+            {
+                title: Event.findByPk(idEvent).name,
+                unit_price: price,
+                currency_id: Event.findByPk(idEvent).currency,
+                quantity: quantity,
+            }
+        ],
+        back_urls: {
+            success: "https://pf-grupo06-back.onrender.com/orderMercadoPago/success",
+            failure: "https://pf-grupo06-back.onrender.com/orderMercadoPago/failure",
+            pending: "https://pf-grupo06-back.onrender.com/orderMercadoPago/pending"
+        },
+        notification_url: "https://6172-186-113-173-20.ngrok.io/webHook"
+
+    });
+
+    const createOrder = await Order.create({
+        idBuyer,
+        idMercadoPago: result.body.id
+    });
+
+    //Agrega a Order_Event con ese createOrder id
+
+    console.log(result)
+        
+    return {response: result.response, preferenceId: result.preferenceId}
 };
 
 module.exports = postCreateOrder;
