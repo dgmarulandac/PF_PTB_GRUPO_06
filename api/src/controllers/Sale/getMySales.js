1f417bbc35d0673889b3f73513d8de200786e544
Gabo <gsarmiento101874@gmail.com>
2023-09-04 01:58:43 -0500

    ajustado el metodo de suma de ventas

--- api/src/controllers/Sale/getMySales.js
+++ api/src/controllers/Sale/getMySales.js
@@ -14,8 +14,8 @@ const getMySales = async (idSeller) => {
     let monthYears = [];
 
     for( let i = 0; i < events.length; i++ ) {
-        const date = new Date(events[i].date);
-        const myString = `${(''+date.getMonth()).length == 1 ? '0'+String(date.getMonth()) : date.getMonth()}-${date.getFullYear()}`;
+        const date = new Date(+events[i].date.split('-')[0], +events[i].date.split('-')[1]-1, +events[i].date.split('-')[2] );
+        const myString = `${(''+(date.getMonth()+1)).length == 1 ? '0'+String((date.getMonth()+1)) : (date.getMonth()+1)}-${date.getFullYear()}`;
         if( monthYears.filter(my => {return my === myString }).length === 0 ) {
             monthYears.push(myString);
         }
@@ -24,14 +24,14 @@ const getMySales = async (idSeller) => {
         }});
         let arr = [];
         for( let j = 0; j < ordersEvent.length; j++ ) {
-            const order = await Order.findByPk( ordersEvent, {
+            const order = await Order.findByPk( ordersEvent[j].dataValues.idOrder, {
                 include: [ {model: Sale, required: true, attributes: ['isSuccesful'] } ]
             } );
-            if( order.dataValues.Sale.dataValues.isSuccesful ) {
+            if( order && order.dataValues.Sale.dataValues.isSuccesful ) {
                 arr.push( {sales: ordersEvent[j].dataValues.quantity * ordersEvent[j].dataValues.price, month: myString})
             }
         }
-        orders = [...orders, ...arr];
+        orders = orders.concat(arr);
     }
 
     const sales = [];
