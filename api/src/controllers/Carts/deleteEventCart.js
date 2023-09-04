1bfa776a10e942628a869fa5b462ec41e9524d50
dgmarulandac <54507534+dgmarulandac@users.noreply.github.com>
2023-09-03 20:13:55 -0500

    update

--- api/src/controllers/Carts/deleteEventCart.js
+++ api/src/controllers/Carts/deleteEventCart.js
@@ -20,7 +20,7 @@ const deleteEventFromCart = async (req, res) => {
     const cartEvent = await Cart_Event.findOne({
       where: {
         idEvent: eventId,
-        
+        idCart: userId, 
       },
     });
 
