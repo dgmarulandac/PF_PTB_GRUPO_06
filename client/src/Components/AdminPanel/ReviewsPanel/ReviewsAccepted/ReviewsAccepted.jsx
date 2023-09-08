a750027b991235f2ad3879114af5857b83aa873e
Cristian-Massa <cristianmassa30@gmail.com>
2023-09-08 03:09:27 -0300

    pag done

--- client/src/Components/AdminPanel/ReviewsPanel/ReviewsAccepted/ReviewsAccepted.jsx
+++ client/src/Components/AdminPanel/ReviewsPanel/ReviewsAccepted/ReviewsAccepted.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
+import axios from "axios";
+import Paginado from "../../../pagination/pagination";
+import * as styles from "../../../EventsDashboard/EventDashboardStiles";
 
 const ReviewsAccepted = () =>{
     const [reviews, setReviews] = useState([])
+    const [currentPage, setCurrentPage] = useState(1);
+    const eventsPerPage = 8;
+
+    const ultimoIndex = currentPage * eventsPerPage;
+    const primerIndex = ultimoIndex - eventsPerPage;
+
+    const [reviewsAMostrar, setReviewsAMostrar] = useState([]);
+
+    useEffect(() => {
+        setCurrentPage(1);
+    }, [reviews]);
+
+    useEffect(() => {
+        setReviewsAMostrar(reviews.slice(primerIndex, ultimoIndex));
+    }, [reviews, currentPage]);
+
     useEffect(() => {
         axios.get('/reviews/admin', { headers: { 'X-Access-Token': localStorage.getItem('jwt') } })
             .then(res => res.data)
@@ -11,27 +30,30 @@ const ReviewsAccepted = () =>{
     }, [])
     const handleClick = (e) => {
         const { value } = e.currentTarget
-        console.log(value)
         axios.put(`/reviews/putReview/${value}`, value, { headers: { 'X-Access-Token': localStorage.getItem('jwt') } })
             .then(res => res.data)
             .then(data => {
-                const reAdded = [...reviews?.filter(element => element.id !== data.id), data];
+                const reAdded = [...reviews?.filter(element => element.id !== data.id)];
+                console.log(reAdded)
                 setReviews(reAdded)
+
             })
     };
-
+    useEffect(()=>{
+        setReviewsAMostrar(reviews.slice(primerIndex, ultimoIndex))
+    }, [reviews])
     return(
                 <div>
             <h1>Panel de Reviews</h1>
-            <div className="grid md:grid-cols-2 grid-cols-1">
+            <div className="grid">
 
                 <div>
                     <h2>Comentarios aceptados:</h2>
                     <div>
 
                     {
-                        reviews?.length > 0 ?
-                            reviews.filter(element => element.approved === true).map((element, key) => {
+                        reviewsAMostrar?.length > 0 ?
+                        reviewsAMostrar.filter(element => element.approved === true).map((element, key) => {
                                 {
                                     return (
                                         <div key={key++} className="odd:bg-slate-800 flex h-20 items-center gap-x-5 space-between justify-between px-5">
@@ -51,8 +73,16 @@ const ReviewsAccepted = () =>{
                         }
                         </div>
                 </div>
-                
+                <div>
+            <Paginado
+                    eventsPerPage={eventsPerPage}
+                    events={reviews}
+                    page={currentPage}
+                    paginado={setCurrentPage}
+                />
+            </div>
             </div>
         </div>
     )
 }
+export default ReviewsAccepted
--- client/src/Components/AdminPanel/ReviewsPanel/ReviewsPanel.jsx
+++ client/src/Components/AdminPanel/ReviewsPanel/ReviewsPanel.jsx
@@ -1,82 +1,18 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+import ReviewsAccepted from "./ReviewsAccepted/ReviewsAccepted";
+import ReviewsBanned from "./ReviewsBanned/ReviewsBanned";
 const ReviewsPanel = () => {
-    const [reviews, setReviews] = useState([])
-    useEffect(() => {
-        axios.get('/reviews/admin', { headers: { 'X-Access-Token': localStorage.getItem('jwt') } })
-            .then(res => res.data)
-            .then(data => {
-                setReviews(data)
-            })
-    }, [])
-    const handleClick = (e) => {
-        const { value } = e.currentTarget
-        console.log(value)
-        axios.put(`/reviews/putReview/${value}`, value, { headers: { 'X-Access-Token': localStorage.getItem('jwt') } })
-            .then(res => res.data)
-            .then(data => {
-                const reAdded = [...reviews?.filter(element => element.id !== data.id), data];
-                setReviews(reAdded)
-            })
-    };
+    const [reviews, setReviews] = useState(true);
+    const handleChange = () =>{
+        setReviews(!reviews)
+    }
     return (
         <div>
-            <h1>Panel de Reviews</h1>
-            <div className="grid md:grid-cols-2 grid-cols-1">
-
-                <div>
-                    <h2>Comentarios aceptados:</h2>
-                    <div>
-
-                    {
-                        reviews?.length > 0 ?
-                            reviews.filter(element => element.approved === true).map((element, key) => {
-                                {
-                                    return (
-                                        <div key={key++} className="odd:bg-slate-800 flex h-20 items-center gap-x-5 space-between justify-between px-5">
-
-                                            <p>{element.comment}</p>
-                                            <div className="flex flex-col">
-                                                <div>
-                                                    <button onClick={handleClick} value={element.id} className="px-5 hover:bg-red-800 bg-red-900 mx-2 rounded-md" style={{ height: '50px' }}>Desactivar</button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    )
-                                }
-                            })
-                            :
-                            null
-                        }
-                        </div>
-                </div>
-                <div>
-                    <h2>Comentarios ocultos:</h2>
-                    <div className="grid">
-
-                        {
-                            reviews?.length > 0 ?
-                                reviews.filter(element => element.approved === false).map((element, key) => {
-                                    {
-                                        return (
-                                            <div key={key++} className="odd:bg-slate-800 flex h-20 items-center gap-x-5 space-between justify-between px-5">
-
-                                                <p>{element.comment}</p>
-                                                <div className="flex flex-col">
-                                                    <div>
-                                                        <button onClick={handleClick} value={element.id} className="py-3 px-8 bg-green-900 hover:bg-green-800 mx-2 rounded-md ">Activar</button>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        )
-                                    }
-                                })
-                                :
-                                null
-                        }
-                    </div>
-                </div>
-            </div>
+            <select className="m-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onChange={handleChange}>
+                <option value={true}>Usuarios activos</option>
+                <option value={false}>Usuarios desactivados</option>
+            </select>
+            {reviews? <ReviewsAccepted /> : <ReviewsBanned /> }
         </div>
     )
 
