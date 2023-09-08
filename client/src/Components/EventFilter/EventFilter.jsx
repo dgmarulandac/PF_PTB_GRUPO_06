07a9a4c211eac002bb6297111068b97bfcefadc8
Gabo <gsarmiento101874@gmail.com>
2023-09-08 17:23:43 -0500

    ajuste

--- client/src/Components/EventFilter/EventFilter.jsx
+++ client/src/Components/EventFilter/EventFilter.jsx
@@ -97,7 +97,7 @@ const EventFilter = () => {
         </select>
   
         <button onClick={handleResetFilters} className={FilStyles.Button}>
-          <span class="relative z-10">Quitar </span>
+          <span class="relative z-10">Remover Filtros</span>
         </button>
 
       </div>
