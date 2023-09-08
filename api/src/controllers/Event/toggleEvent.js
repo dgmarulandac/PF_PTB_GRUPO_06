8ff496005969dd1e7adf3548c517ad03b53dd810
Gabo <gsarmiento101874@gmail.com>
2023-09-08 18:27:06 -0500

    ajuste bug dani

--- api/src/controllers/Event/toggleEvent.js
+++ api/src/controllers/Event/toggleEvent.js
@@ -8,7 +8,7 @@ const toggleEvent = async (id, userInfo) => {
     }
 
     if( userInfo.isSeller && !userInfo.isAdmin ) {
-        if( event.idSeller !== userInfo.id ) {
+        if( event.idSeller !== userInfo.userId ) {
             throw Error("Este evento no le pertenenece.");
         }
     }
