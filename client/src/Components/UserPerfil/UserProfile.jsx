689cb1ba36e3a9c338e202c8fb6f633c555e9131
Daniel Lopez <daniel.l1.g@hotmail.com>
2023-09-08 00:02:48 -0500

    reparado

--- client/src/Components/UserPerfil/UserProfile.jsx
+++ client/src/Components/UserPerfil/UserProfile.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { putUserProfile } from "../../Redux/Action/action";
-import styles from "./userProfile.module.css"; // Importa los estilos CSS Modules
 
 export default function UserProfile() {
   const user = useSelector((state) => state.userSesion);
@@ -66,7 +65,7 @@ export default function UserProfile() {
               className="mx-auto shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
             />
           </div>
-          <div className={styles.labelContainer}>
+          <div >
             <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
               Nombre completo:
             </label>
@@ -79,7 +78,7 @@ export default function UserProfile() {
               className="mx-auto shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
             />
           </div>
-          <div className={styles.labelContainer}>
+          <div >
             <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
               Contraseña:
             </label>
@@ -92,7 +91,7 @@ export default function UserProfile() {
               className="mx-auto shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
             />
           </div>
-          <div className={styles.labelContainer}>
+          <div >
             <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
               Email:
             </label>
@@ -105,7 +104,7 @@ export default function UserProfile() {
               className="mx-auto shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
             />
           </div>
-          <div className={styles.labelContainer}>
+          <div >
             <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
               Dirección:
             </label>
@@ -118,7 +117,7 @@ export default function UserProfile() {
               className="mx-auto shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
             />
           </div>
-          <div className={styles.labelContainer}>
+          <div >
             <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
               Nacionalidad:
             </label>
@@ -131,7 +130,7 @@ export default function UserProfile() {
               className="mx-auto shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
             />
           </div>
-          <div className={styles.labelContainer}>
+          <div >
             <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
               Teléfono:
             </label>
