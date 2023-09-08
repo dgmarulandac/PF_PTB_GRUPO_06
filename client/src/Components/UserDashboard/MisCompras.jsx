887d17bdee86b5d5f31387278b2e29bbbd770d5e
Giann92 <56614039+Giann92@users.noreply.github.com>
2023-09-08 01:10:12 -0300

    MisComprasCorreciones

--- client/src/Components/UserDashboard/MisCompras.jsx
+++ client/src/Components/UserDashboard/MisCompras.jsx
@@ -4,59 +4,76 @@ import { Link } from "react-router-dom";
 import styles from "./misCompras.module.css"; // Importa el archivo de módulo CSS
 
 const MisCompras = () => {
-    const exampleSales = [
-        { id: 1, purchaseNumber: "ABC123", eventName: "Festival de Cine-1", price: 15.5, quantity: 2, isCompra: true, date: "2023-06-20", image: "cine.jpg" },
-        { id: 2, purchaseNumber: "DEF456", eventName: "Festival de Cine-2", price: 15.5, quantity: 1, isCompra: true, date: "2023-07-20", image: "cine.jpg" },
-        { id: 3, purchaseNumber: "GHI789", eventName: "Concierto de Rock", price: 25.99, quantity: 3, isCompra: true, date: "2023-08-15", image: "concierto.jpg" },
-        { id: 4, purchaseNumber: "JKL012", eventName: "Exposición de Arte", price: 10.0, quantity: 10, isCompra: true, date: "2023-08-05", image: "arte.jpg" },
-    ];
-    
-    const mySales = exampleSales.filter((sale) => sale.isCompra);
+  const myPurchases = useSelector((state) => state.myPurchases);
 
-    const salesByMonth = mySales.reduce((acc, sale) => {
-        const month = new Date(sale.date).toLocaleDateString('en-US', { month: 'long', year: 'numeric' });
-        if (!acc[month]) {
-            acc[month] = [];
-        }
-        acc[month].push(sale);
-        return acc;
-    }, {});
+  const purchases = myPurchases.filter((sale) => sale.isCompra);
 
-    return (
-        <div>
-            <h1 className={styles.heading}>Mis Compras</h1>
-            {Object.entries(salesByMonth).map(([month, sales]) => (
-                <div key={month} className={styles.monthContainer}>
-                    <h2>{month}</h2>
-                    <table className={styles.salesTable}>
-                        <thead>
-                            <tr>
-                                <th>Número de Compra</th>
-                                <th>Evento</th>
-                                <th>Precio</th>
-                                <th>Cantidad</th>
-                                <th>Total</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {sales.map((sale) => (
-                                <tr key={sale.id}>
-                                    <td>Compra #{sale.purchaseNumber}</td>
-                                    <td>{sale.eventName}</td>
-                                    <td>${sale.price}</td>
-                                    <td>{sale.quantity}</td>
-                                    <td>${sale.price * sale.quantity}</td>
-                                    <td><img src={sale.image} alt={sale.eventName} className={styles.eventImage} /></td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            ))}
+  const salesByMonth = purchases.reduce((acc, sale) => {
+    const month = new Date(sale.date).toLocaleDateString("en-US", {
+      month: "long",
+      year: "numeric",
+    });
+    if (!acc[month]) {
+      acc[month] = [];
+    }
+    acc[month].push(sale);
+    return acc;
+  }, {});
 
-            <Link to='/'> <button className={styles.backButton}>Back</button></Link>
+  // Verificar si no hay compras realizadas
+  const noPurchasesMessage =
+    Object.keys(salesByMonth).length === 0 ? (
+      <div className={styles.noPurchasesMessage}>
+        No tienes compras realizadas hasta el momento.
+        No seas rata compra..
+      </div>
+    ) : null;
+
+  return (
+    <div>
+      <h1 className={styles.heading}>Mis Compras</h1>
+      {noPurchasesMessage}
+      {Object.entries(salesByMonth).map(([month, sales]) => (
+        <div key={month} className={styles.monthContainer}>
+          <h2>{month}</h2>
+          <table className={styles.salesTable}>
+            <thead>
+              <tr>
+                <th>Número de Compra</th>
+                <th>Evento</th>
+                <th>Precio</th>
+                <th>Cantidad</th>
+                <th>Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              {sales.map((sale) => (
+                <tr key={sale.id}>
+                  <td>Compra #{sale.purchaseNumber}</td>
+                  <td>{sale.eventName}</td>
+                  <td>${sale.price}</td>
+                  <td>{sale.quantity}</td>
+                  <td>${sale.price * sale.quantity}</td>
+                  <td>
+                    <img
+                      src={sale.image}
+                      alt={sale.eventName}
+                      className={styles.eventImage}
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-    );
+      ))}
+
+      <Link to="/">
+        {" "}
+        <button className={styles.backButton}>Back</button>
+      </Link>
+    </div>
+  );
 };
 
 export default MisCompras;
--- client/src/Redux/Reducer/reducer.js
+++ client/src/Redux/Reducer/reducer.js
@@ -1,4 +1,4 @@
-import { GET_ALL_EVENT, CREATE_EVENT, GET_DETAIL, FILTER_GET_EVENTS, POST_LOGIN, MODAL, LOG_OUT, ORDER_PAY, GET_MY_EVENTS, PUT_EVENT, GET_MY_SALES, POST_REVIEW, ADD_CAR, GET_EVENTS_ADMIN, ADD_TO_CAR, PLUS_LESS, DELETE_EVENT_CAR, SEARCH_EVENT_ADMIN } from "../Action/action-type";
+import { GET_ALL_EVENT, CREATE_EVENT, GET_DETAIL, FILTER_GET_EVENTS, POST_LOGIN, MODAL, LOG_OUT, ORDER_PAY, GET_MY_EVENTS, PUT_EVENT, GET_MY_SALES, POST_REVIEW, ADD_CAR, GET_EVENTS_ADMIN, ADD_TO_CAR, PLUS_LESS, DELETE_EVENT_CAR, SEARCH_EVENT_ADMIN, GET_MY_PURCHASES } from "../Action/action-type";
 
 const initialState = {
     country: ['Colombia', 'Venezuela', 'Argentina', 'Uruguay'],
@@ -12,7 +12,8 @@ const initialState = {
     mySales: [],
     preferenceId: false,
     shoppingCar: [],
-    eventsAdmin: []
+    eventsAdmin: [],
+    myPurchases: []
 }
 
 const rootReducer = (state = initialState, action)=>{
@@ -52,6 +53,8 @@ const rootReducer = (state = initialState, action)=>{
             return {...state, eventsAdmin: payload}
         case SEARCH_EVENT_ADMIN:
             return {...state, eventsAdmin: payload}
+        case GET_MY_PURCHASES:
+            return {...state , myPurchases: payload}
         default: 
             return state;
     }
